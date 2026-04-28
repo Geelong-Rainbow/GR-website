a0f8e42967920d3ec689f144f8473a1959d4a332
Friedslick6 <business@geelongrainbow.org.au>
2026-04-28 17:26:27 +1000

    Carousel scroll animation improved

--- script.js
+++
@@ -1,7 +0,0 @@
-function randomizeCarouselScroll() {
-  document.querySelectorAll('.carousel-image').forEach(image => {
-      image.style.animationDelay = `${Math.random() * 100}%`;
-  });
-}
-
-window.onload = randomizeCarouselScroll;
